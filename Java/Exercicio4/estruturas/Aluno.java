ff91db1b29ad2b83c599c1ffb9f9be4ea04cbace
Leonardo Bissani <7514664+leobissani@users.noreply.github.com>
2017-10-28 12:52:25 -0200

    Add files via upload

--- Java/Exercicio4/estruturas/Aluno.java
+++ Java/Exercicio4/estruturas/Aluno.java
@@ -1,1 +1,63 @@
+package estruturas;
+import java.util.Date;
 
+public class Aluno extends Pessoa {
+
+    public enum NIVEL {
+        INDEFINIDO,
+        GRADUAÇÃO,
+        ESPECIALIZAÇÃO,
+        MESTRADO,
+        DOUTORADO
+    };
+
+    private String codigo;
+    private NIVEL nivel;
+
+    public Aluno() {
+        this.setCodigo("00000000");
+        this.setNivel(NIVEL.INDEFINIDO);
+    }
+
+    public Aluno(String nome, Date dtnascimento, SEXO sexo, String codigo, NIVEL nivel) {
+        super(nome, dtnascimento, sexo);
+        this.setCodigo(new Aluno().getCodigo());
+        this.setCodigo(codigo);
+        this.setNivel(nivel);
+    }
+
+    public Aluno(Aluno a) {
+        this(a.getNome(), a.getDtnascimento(), a.getSexo(), a.getCodigo(), a.getNivel());
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        if (codigo.matches("[0-9]+") && codigo.length() > 1)
+        	this.codigo = codigo;
+    }
+
+    public NIVEL getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(NIVEL nivel) {
+        this.nivel = nivel;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer resultado = new StringBuffer();
+        
+        resultado.append("\nAluno");
+        resultado.append(super.toString()+",\n\t");
+        resultado.append("\n\t{");
+        resultado.append("\n\t\tCodigo: "+this.getCodigo());
+        resultado.append("\n\t\tNivel: "+ this.getNivel());
+        resultado.append("\n\t}");
+
+        return resultado.toString();
+    }
+}
---
+++ Java/Exercicio4/estruturas/Main.java
@@ -0,0 +1,44 @@
+package estruturas;
+
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import estruturas.*;
+import estruturas.Pessoa.SEXO;
+
+public class Main {
+
+    private static SimpleDateFormat auxiliarData;        
+
+    public static void exercicio1a() throws ParseException {
+    	
+    	Pessoa[] listaPessoas = new Pessoa[12];
+    	
+    	listaPessoas[0] = new Pessoa("Danny Carey", auxiliarData.parse("10/05/1961"), Pessoa.SEXO.MASCULINO);
+    	listaPessoas[1] = new Pessoa("Adam Jones", auxiliarData.parse("15/01/1965"), Pessoa.SEXO.MASCULINO);
+    	listaPessoas[2] = new Pessoa("Maynard James Keenan", auxiliarData.parse("17/04/1964"), Pessoa.SEXO.MASCULINO);
+    	listaPessoas[3] = new Pessoa("Adam Jones", auxiliarData.parse("19/09/1971"), Pessoa.SEXO.MASCULINO);
+    	listaPessoas[4] = new Aluno("Leonardo Bissani", auxiliarData.parse("13/06/1989"), Pessoa.SEXO.MASCULINO, "220485", Aluno.NIVEL.GRADUAÇÃO);
+    	listaPessoas[5] = new Aluno("Arnaldo Antunes", auxiliarData.parse("07/03/1983"), Pessoa.SEXO.MASCULINO, "217484", Aluno.NIVEL.MESTRADO);
+    	listaPessoas[6] = new Aluno("Bárbara Broetto", auxiliarData.parse("04/11/1960"), Pessoa.SEXO.FEMININO, "183250", Aluno.NIVEL.DOUTORADO);
+    	listaPessoas[7] = new Aluno("César Coelho", auxiliarData.parse("10/09/1956"), Pessoa.SEXO.FEMININO, "189520", Aluno.NIVEL.DOUTORADO);
+    	listaPessoas[8] = new Funcionario("Jorge Junqueira", auxiliarData.parse("11/12/1980"), Pessoa.SEXO.MASCULINO, new Date(), Funcionario.CATEGORIA.PROGRAMADOR);
+    	listaPessoas[9] = new Funcionario("Kathleen Kronenberg", auxiliarData.parse("26/05/1995"), Pessoa.SEXO.FEMININO, new Date(), Funcionario.CATEGORIA.ANALISTA);
+    	listaPessoas[10] = new Funcionario("Laís Loureiro", auxiliarData.parse("02/03/1977"), Pessoa.SEXO.FEMININO, new Date(), Funcionario.CATEGORIA.GERENTE);
+    	listaPessoas[11] = new Funcionario("Milton Macedo", auxiliarData.parse("22/08/1999"), Pessoa.SEXO.FEMININO, new Date(), Funcionario.CATEGORIA.INDEFINIDO);
+
+    	for(Pessoa pessoas : listaPessoas) {
+    		System.out.println(pessoas);
+    	}
+    	
+    }
+    
+    
+    public static void main(String[] args) throws ParseException {
+        auxiliarData = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("\n");
+        exercicio1a();
+    }
+
+}
